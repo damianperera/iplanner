e0b4b84b91ef4e152e78697ccf879c41a3b7d6c5
Damian Perera <damian@perera.io>
2018-05-28 19:22:37 +0530

    Set task edit to popover

--- iPlanner/iPlanner/DetailViewController.swift
+++ iPlanner/iPlanner/DetailViewController.swift
@@ -85,39 +85,6 @@ extension DetailViewController: UITableViewDelegate, UITableViewDataSource {
         }
     }
     
-    func controllerWillChangeContent(_ controller: NSFetchedResultsController<NSFetchRequestResult>) {
-        tableView.beginUpdates()
-    }
-    
-    func controller(_ controller: NSFetchedResultsController<NSFetchRequestResult>, didChange sectionInfo: NSFetchedResultsSectionInfo, atSectionIndex sectionIndex: Int, for type: NSFetchedResultsChangeType) {
-        switch type {
-        case .insert:
-            tableView.insertSections(IndexSet(integer: sectionIndex), with: .fade)
-        case .delete:
-            tableView.deleteSections(IndexSet(integer: sectionIndex), with: .fade)
-        default:
-            return
-        }
-    }
-
-    func controller(_ controller: NSFetchedResultsController<NSFetchRequestResult>, didChange anObject: Any, at indexPath: IndexPath?, for type: NSFetchedResultsChangeType, newIndexPath: IndexPath?) {
-        switch type {
-        case .insert:
-            tableView.insertRows(at: [newIndexPath!], with: .fade)
-        case .delete:
-            tableView.deleteRows(at: [indexPath!], with: .fade)
-        case .update:
-            configureCell(tableView.cellForRow(at: indexPath!) as! TaskTableViewCell, withTask: anObject as! Task)
-        case .move:
-            configureCell(tableView.cellForRow(at: indexPath!)! as! TaskTableViewCell, withTask: anObject as! Task)
-            tableView.moveRow(at: indexPath!, to: newIndexPath!)
-        }
-    }
-    
-    func controllerDidChangeContent(_ controller: NSFetchedResultsController<NSFetchRequestResult>) {
-        tableView.endUpdates()
-    }
-    
 }
 
 class DetailViewController: UIViewController, AddCourseworkDelegate, NSFetchedResultsControllerDelegate, AddTaskViewDelegate {
